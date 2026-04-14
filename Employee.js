9c5eaf875d92b87d0cb7db91e12cdfa2145ef46b
benjaminbrown514-cmyk <benjaminbrown514@gmail.com>
2026-04-14 16:15:14 -0700

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,117 +1,63 @@
-
-const inquirer = require('inquirer'); 
-const fs = require('fs');
-
-const fileName = 'TeamPortfolio.html'
-
-
-const question = [
-        {
-        type:'input',
-        message: 'please provide your name:', 
-        name: 'name',
-
-        },
-    
-        {
-        type:'input',
-        message: 'please provide your id:',
-        name: 'id',
-        }, 
-        {
-        type: 'input',
-        message: 'please provide your email:',
-        name: 'email',
-       },
-
-]
-
-
-
-    function generatePortfolio(){
-        inquirer 
-         .prompt(question)
-         .then((answer) => { 
-            retrieveAnswers(answer)
-        })
-    }
-    
-    
-
-    
-    
-
-    generatePortfolio()
-
-    
-
-
-
-
-
-
-    
-    
-
-
-    
-
-
-
 class Employee {
-    constructor(name, id, email,){
+    constructor(name, id, email){
         this.name = name;
         this.id = id;
         this.email = email; 
+        
+        
+    }
+
+    
+//corrected by RB - no space between get and Name
+    getName() {
+        
+        return this.name
+
+
 
     }
 
-    //getName() {
-        
-        
-
-
-
-//}
-
-}
-
-     //getId(){
-        
-     //}
-
-    //getEmail(){
-        
-
-    //}
-
-    //getRole(){
-
-    //}
-//}
-//const employee = new Employee();
-
-//employee.getName();
-
-//employee.getId();
-
-
-
-
-function retrieveAnswers(answers){
+    //corrected by RB - no space between get and id
     
-    var html =  '<!DOCTYPE html> <html lang="en"> <head> <link rel = "stylesheet" href="style.css"></head><body><header class = "header">My Team </header><main> <div id = "nameSection" id = "idSection" id = "emailSection"><div class = "message"> Name: '+ answers.name + ' <span id = "stock"></span></div><div class = "message">id: ' + answers.id + ' <span id = "stock"></div>email: ' + answers.email + ' </main></body></html>'
+    getId(){
+        
+        return this.id
+     
+    }
+    
+    
+    //corrected by RB - no space between get and email
+    getEmail(){
 
-    fs.writeFileSync(fileName, html)
+        return this.email
+        
+
+    }
+
+//corrected by RB - no space between get and role
+    getRole(){
+        return 'Employee'
+
+    }
 
 }
 
+//****added by RB
+module.exports = Employee;
+    
+    
+    
 
 
 
 
-//employee.getID();
-//employee.getEmail();
+
+
+
+
+
+
+
 
 
 
